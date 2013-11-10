b6678a21b6bd018356fc7411c134fe336eabb590
Dominick Piaquadio <dominick@deadbear.(none)>
2013-11-10 08:02:57 -0500

    first commit for django front end

---
+++ socialjusticefe/socialjusticefe/socialjustice/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+# Create your views here.
+def frontend(request):
+    r = requests.get('http://wikipaltz.org/wiki/Events')
+    soup = BeautifulSoup(r.text)
+    
+    time = str(soup.find(id="Time").get_text())
+    
+
+    return render(request, 'test.html', locals())
