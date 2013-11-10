9af6309823b6588403bd51a8e061c7fb6d00a20f
Dominick Piaquadio <dominick@deadbear.(none)>
2013-11-10 08:23:49 -0500

    Fixed AttributeError. Needs its butt wiped'

--- socialjusticefe/socialjusticefe/socialjustice/views.py
+++ socialjusticefe/socialjusticefe/socialjustice/views.py
@@ -6,7 +6,7 @@ def frontend(request):
     r = requests.get('http://wikipaltz.org/wiki/Events')
     soup = BeautifulSoup(r.text)
     
-    time = str(soup.find(id="Time").get_text())
+    time = str(soup.find(id='mw-content-text').get_text())
     
 
     return render(request, 'test.html', locals())
